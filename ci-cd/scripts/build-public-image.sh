7345eec627b05e708a17a1f0527397609b8b104c
Youssef Dhraief <dhraief.youssef@gmail.com>
2020-12-14 20:52:16 +0100

    Changed CI

--- ci-cd/scripts/build-public-image.sh
+++ ci-cd/scripts/build-public-image.sh
@@ -2,6 +2,6 @@
 
 set -x # echo on
 
-sed -e "s/registry.gitlab.com\///g" -i "${DOCKER_IMAGE_PATH}"
+sed -e "s/registry.gitlab.com\/piximos\/scripted-atoms\//piximos\//g" -i "${DOCKER_IMAGE_PATH}"
 docker build --cache-from "${IMAGE_NAME}:${IMG_TAG}" -t "${IMAGE_NAME}:${IMG_TAG}" -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
 docker push "${IMAGE_NAME}:${IMG_TAG}"
