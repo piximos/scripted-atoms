dab6ce488db300fbe90b37078d982720c5f6a6bc
Youssef Dhraief <dhraief.youssef@gmail.com>
2020-12-14 20:45:15 +0100

    Changed mirror builds

--- ci-cd/scripts/build-public-image.sh
+++ ci-cd/scripts/build-public-image.sh
@@ -3,5 +3,5 @@
 set -x # echo on
 
 sed -e "s/registry.gitlab.com\///g" -i "${DOCKER_IMAGE_PATH}"
-docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
-docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+docker build --cache-from "${IMAGE_NAME}:${IMG_TAG}" -t "${IMAGE_NAME}:${IMG_TAG}" -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
+docker push "${IMAGE_NAME}:${IMG_TAG}"
