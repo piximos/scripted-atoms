31f79de69e8b444aa3143afde9be931b7b0a085a
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-02-17 15:53:00 +0100

    Testing mirror

--- ci-cd/scripts/build-image.sh
+++ ci-cd/scripts/build-image.sh
@@ -6,6 +6,7 @@ docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
   -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
   -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
 docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+echo "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" >> ./output.txt
 
 if [[ $BUILD_VERSION && $BUILD_VERSION == "true" ]]; then
   version="$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')"
@@ -13,4 +14,5 @@ if [[ $BUILD_VERSION && $BUILD_VERSION == "true" ]]; then
     -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}" \
     -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
   docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}"
+  echo "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}" >> ./output.txt
 fi
--- ci-cd/scripts/build-public-image.sh
+++ ci-cd/scripts/build-public-image.sh
@@ -5,3 +5,5 @@ set -x # echo on
 sed -e "s/registry.gitlab.com\/piximos\/scripted-atoms\//piximos\//g" -i "${DOCKER_IMAGE_PATH}"
 docker build --cache-from "${IMAGE_NAME}:${IMG_TAG}" -t "${IMAGE_NAME}:${IMG_TAG}" -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
 docker push "${IMAGE_NAME}:${IMG_TAG}"
+
+echo "${IMAGE_NAME}:${IMG_TAG}" >> ./output.txt
