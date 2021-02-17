dfb75e76cd808fbf94c7f030a2b1f33e5b865354
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-02-17 17:02:05 +0100

    Testing clean

--- ci-cd/scripts/clean-image.sh
+++ ci-cd/scripts/clean-image.sh
@@ -2,8 +2,10 @@
 
 set -x # echo on
 
-docker pull "${CI_REGISTRY}/${SRC_IMAGE_NAME}:${IMG_TAG}"
-docker tag "${CI_REGISTRY}/${SRC_IMAGE_NAME}:${IMG_TAG}" "${IMAGE_NAME}:${IMG_TAG}"
-docker rmi "${IMAGE_NAME}:${IMG_TAG}"
-version="$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')"
-docker rmi "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}" 2>/dev/null
+cat ./output.txt
+
+#docker pull "${CI_REGISTRY}/${SRC_IMAGE_NAME}:${IMG_TAG}"
+#docker tag "${CI_REGISTRY}/${SRC_IMAGE_NAME}:${IMG_TAG}" "${IMAGE_NAME}:${IMG_TAG}"
+#docker rmi "${IMAGE_NAME}:${IMG_TAG}"
+#version="$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')"
+#docker rmi "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}" 2>/dev/null
