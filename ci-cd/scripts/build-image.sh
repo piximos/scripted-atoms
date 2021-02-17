5b46270567a2ec0600a2cb867f86e75ac5b96057
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-02-17 21:44:10 +0100

    Changed rules for building images

--- ci-cd/scripts/build-image.sh
+++ ci-cd/scripts/build-image.sh
@@ -2,17 +2,17 @@
 
 set -x # echo on
 
-docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
-  -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
-  -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
-docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
-echo "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" >> ./output.txt
-
-if [[ $BUILD_VERSION && $BUILD_VERSION == "true" ]]; then
-  version="$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')"
-  docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
-    -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}" \
-    -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
-  docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}"
-  echo "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}" >> ./output.txt
+if [[ $CI_COMMIT_REF_NAME != "master" ]]; then
+  IMG_TAGS=("$CI_COMMIT_REF_NAME")
+else
+  IMG_TAGS=("latest" "$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')")
 fi
+
+for IMG_TAG in "${IMG_TAGS[@]}"; do
+  echo "Building image : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+  docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
+    -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
+    -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
+  docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+  echo "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" >>./output.txt
+done
