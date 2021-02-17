1126e5308d8ddb21b4b9c34effcf9c22e4ad3577
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-02-17 14:52:23 +0100

    added versioned build

--- ci-cd/scripts/build-image.sh
+++ ci-cd/scripts/build-image.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x # echo on
 
-docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
+docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
+  -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
+  -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
 docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+
+if [[ $BUILD_VERSION && $BUILD_VERSION == "true" ]]; then
+  version="$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)')"
+  docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
+    -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}" \
+    -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
+  docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${version}"
+fi
