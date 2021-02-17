13e17450c13dbf408f5dcfa55245da31f0ea8476
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-02-17 22:07:31 +0100

    Changed rules for building images

--- ci-cd/scripts/build-image.sh
+++ ci-cd/scripts/build-image.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-set -x # echo on
-
+MIRROR="false"
 if [[ $CI_COMMIT_REF_NAME != "master" ]]; then
   IMG_TAGS=("$CI_COMMIT_REF_NAME")
 else
+  MIRROR="true"
   IMG_TAGS=("latest" "$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')")
 fi
 
 for IMG_TAG in "${IMG_TAGS[@]}"; do
+
   echo "Building image : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
   docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
     -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
     -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
   docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
   echo "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" >>./output.txt
+
+  if [[ $MIRROR == "true" && $PUBLIC_IMAGE_NAME ]]; then
+      echo "Building public image : ${DOCKER_REGISTRY}/${PUBLIC_IMAGE_NAME}:${IMG_TAG}"
+      sed -e "s/registry.gitlab.com\/piximos\/scripted-atoms\//piximos\//g" -i "${DOCKER_IMAGE_PATH}"
+      docker build --cache-from "${PUBLIC_IMAGE_NAME}:${IMG_TAG}" -t "${PUBLIC_IMAGE_NAME}:${IMG_TAG}" -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
+      docker push "${PUBLIC_IMAGE_NAME}:${IMG_TAG}"
+
+      echo "${PUBLIC_IMAGE_NAME}:${IMG_TAG}" >> ./output.txt
+  fi
 done
