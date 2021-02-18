8174ca99d120454fbca594f188f017169bca67dc
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-02-18 09:39:22 +0100

    Edited clean script to delete untagged images and leave latest in cache

--- ci-cd/scripts/clean-image.sh
+++ ci-cd/scripts/clean-image.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-set -x # echo on
-
+IMG_TAGS+=("<none>")
 if [[ $CI_COMMIT_REF_NAME != "master" ]]; then
-  IMG_TAGS=("$CI_COMMIT_REF_NAME")
+  IMG_TAGS+=("$CI_COMMIT_REF_NAME")
 else
-  IMG_TAGS=("latest" "$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')")
+  IMG_TAGS+=("$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')")
 fi
 
 for IMG_TAG in "${IMG_TAGS[@]}"; do
   docker images \
   | grep -E '^(registry\.gitlab\.com\/)?piximos' \
+  | awk 'NR>1 {img_name=sprintf("%s:%s",$1,$2); print img_name}' \
   | grep "scripted" \
   | grep "atom" \
   | grep "$IMG_TAG" \
-  | 'NR>1 {img_name=sprintf("%s:%s",$1,$2); print img_name}' \
   | xargs docker rmi
 done
