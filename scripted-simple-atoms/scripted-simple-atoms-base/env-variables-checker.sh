7be0a197ac4917074196223c4e9ea23b19496c54
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-04-05 15:53:07 +0100

    Added s3 file backup ssa

--- scripted-simple-atoms/scripted-simple-atoms-base/env-variables-checker.sh
+++ scripted-simple-atoms/scripted-simple-atoms-base/env-variables-checker.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+env_variable_value_validator() {
+  values="$(echo "${1}" | jq -r '. | @sh' | tr -d \')"
+  values=($values)
+  var_name="${2}"
+  var_value="${!var_name}"
+
+  if [[ ! " ${values[*]} " =~ " ${var_value} " ]]; then
+    echo "The value [$var_value] of the variable '$var_name' must be on of the following : ${values[*]}"
+    exit 1
+  fi
+}
+
 env_variable_validator() {
   var_name="$(echo "${1}" | jq -r .name)"
   var_required="$(echo "${1}" | jq -r .required)"
   var_description="$(echo "${1}" | jq -r .desc)"
+  var_values="$(echo "${1}" | jq -r .values)"
   if [[ $var_required == "true" && -z "${!var_name}" ]]; then
     echo "The variable '$var_name' [$var_description] is not set. "
     exit 1
+  fi
+
+  if [[ -n "${var_values}" && "${var_values}" != "null" ]]; then
+    env_variable_value_validator "$var_values" "$var_name"
   fi
 }
 
