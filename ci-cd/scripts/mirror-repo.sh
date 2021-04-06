993c09663e0fdc738237d0cc56d28a554e508d63
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-04-06 21:09:58 +0100

    Started adding git sync stage

---
+++ ci-cd/scripts/mirror-repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker pull piximos/scripted-simple-atom-git-mirror-atom:latest
+docker run --rm \
+  -e ORIGIN_REPO_URI="$CURRENT_REPO_URI" \
+  -e ORIGIN_REPO_USER="$CURRENT_REPO_USER" \
+  -e ORIGIN_REPO_TOKEN="$CURRENT_REPO_TOKEN" \
+  -e REMOTE_REPO_URI="$REMOTE_REPO_URI" \
+  -e REMOTE_REPO_USER="$REMOTE_REPO_USER" \
+  -e REMOTE_REPO_TOKEN="$REMOTE_REPO_TOKEN" \
+  piximos/scripted-simple-atom-git-mirror-atom:latest
