d607f8f18f1145059cfad956f5bc986babe3d2cc
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-04-06 20:29:32 +0100

    Added arm images build

--- ci-cd/scripts/build-image.sh
+++ ci-cd/scripts/build-image.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 MIRROR="false"
+if [[ -z ${BUILD_FOR_PRIVATE_REGISTRY} ]]; then
+  BUILD_FOR_PRIVATE_REGISTRY="true"
+fi
 if [[ $CI_COMMIT_REF_NAME != "master" ]]; then
-  IMG_TAGS=("$CI_COMMIT_REF_NAME")
+  IMG_TAGS=("$CI_COMMIT_REF_NAME$IMG_COMPLEMENTARY_TAG")
 else
   MIRROR="true"
-  IMG_TAGS=("latest" "$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')")
+  IMG_TAGS=("latest$IMG_COMPLEMENTARY_TAG" "$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')$IMG_COMPLEMENTARY_TAG")
 fi
 
 for IMG_TAG in "${IMG_TAGS[@]}"; do
 
-  echo "Building image : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+  echo "Building image for private registry : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
   docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:latest" \
     -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
     -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}" \
     > /dev/null
-  echo "Pushing image : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+  echo "Pushing image to private registry : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
   docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
     > /dev/null
-  echo "Pushed image : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+  echo "Pushed image to private registry : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
 
   if [[ $MIRROR == "true" && $PUBLIC_IMAGE_NAME ]]; then
     echo "Building public image : ${PUBLIC_IMAGE_NAME}:${IMG_TAG}"
