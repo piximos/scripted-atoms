36376f1af5f5437d73f01ffd805d7fcd3feeb34d
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-04-07 09:10:36 +0100

    Edited workflow

--- ci-cd/scripts/build-image.sh
+++ ci-cd/scripts/build-image.sh
@@ -3,15 +3,18 @@
 if [[ -z ${BUILD_FOR_PRIVATE_REGISTRY} ]]; then
   BUILD_FOR_PRIVATE_REGISTRY="true"
 fi
-if [[ $CI_COMMIT_REF_NAME != "stable-release" ]]; then
+if [[ $CI_COMMIT_REF_NAME != "major-release" && $CI_COMMIT_REF_NAME != "minor-release" && $CI_COMMIT_REF_NAME != "master" ]]; then
   IMG_TAGS=("$CI_COMMIT_REF_NAME$IMG_COMPLEMENTARY_TAG")
-else
+elif [[ $CI_COMMIT_REF_NAME = "master" ]]; then
   IMG_TAGS=("latest$IMG_COMPLEMENTARY_TAG" "$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')$IMG_COMPLEMENTARY_TAG")
+else
+  IMG_TAGS=("$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')$IMG_COMPLEMENTARY_TAG")
 fi
 
 for IMG_TAG in "${IMG_TAGS[@]}"; do
   if [[ $BUILD_FOR_PRIVATE_REGISTRY = "true" ]]; then
   echo "Building image for private registry : ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}"
+    sed -e "s:scriptedatom/:$CI_REGISTRY/piximos/scripted-atoms/:g" -i "${DOCKER_IMAGE_PATH}"
   docker build --cache-from "${DOCKER_REGISTRY}/${IMAGE_NAME}:latest" \
     -t "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMG_TAG}" \
     -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
@@ -22,7 +25,6 @@ for IMG_TAG in "${IMG_TAGS[@]}"; do
 
   if [[ $MIRROR == "true" && $PUBLIC_IMAGE_NAME ]]; then
     echo "Building public image : ${PUBLIC_IMAGE_NAME}:${IMG_TAG}"
-    sed -e "s:$CI_REGISTRY/piximos/scripted-atoms/:piximos/:g" -i "${DOCKER_IMAGE_PATH}"
     docker build --cache-from "${PUBLIC_IMAGE_NAME}:latest" \
       -t "${PUBLIC_IMAGE_NAME}:${IMG_TAG}" \
       -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
--- ci-cd/scripts/clean-image.sh
+++ ci-cd/scripts/clean-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMG_TAGS+=("<none>")
-if [[ $CI_COMMIT_REF_NAME != "stable-release" ]]; then
+if [[ $CI_COMMIT_REF_NAME != "major-release" && $CI_COMMIT_REF_NAME != "minor-release" && $CI_COMMIT_REF_NAME != "master" ]]; then
   IMG_TAGS+=("$CI_COMMIT_REF_NAME")
 else
   IMG_TAGS+=("$(git describe | grep -Eo '^([0-9]+\-[0-9]+\-[0-9]+)' | tr '-' '.')")
@@ -10,7 +10,7 @@ fi
 for IMG_TAG in "${IMG_TAGS[@]}"; do
   echo "Deleting the following images : \
     $(docker images |
-    grep -E '^(registry\.gitlab\.com\/)?piximos' |
+    grep -E '^(registry\.gitlab\.com\/piximos)|(scriptedatom\/)' |
     awk 'NR>1 {img_name=sprintf("%s:%s",$1,$2); print img_name}' |
     grep "scripted" |
     grep "atom" |
