41e241b1e33bdef8e219f8049ea22357c01217e8
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-06-15 11:49:48 +0100

    Edited image builds

---
+++ ci-cd/scripts/build-public-image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ -z ${SA_BASE_VERSION} ]]; then
+  echo "Missing 'SA_BASE_VERSION' variable"
+  exit 1
+fi
+
+echo "Building 'latest' tag."
+DOCKER_BUILDKIT=1 docker build \
+  --build-arg SA_BASE_VERSION="${SA_BASE_VERSION}" \
+  -t "${IMAGE_NAME}" \
+  -f "${DOCKER_IMAGE_PATH}" "${DOCKER_BUILD_CONTEXT}"
+
+echo "Pushing 'latest' tag."
+docker push "${IMAGE_NAME}"
+echo "Pushed public image : ${IMAGE_NAME}:${IMG_TAG}"
+
+if [[ ${IMAGE_VERSION} ]]; then
+  echo "Tagging versioned image."
+  docker tag \
+    "${IMAGE_NAME}:latest" \
+    "${IMAGE_NAME}:${IMAGE_VERSION}"
+
+  echo "Pushing image : ${IMAGE_NAME}:${IMG_TAG}"
+
+  docker push "${IMAGE_NAME}:${IMG_TAG}"
+  echo "Pushed public image : ${IMAGE_NAME}:${IMG_TAG}"
+fi
