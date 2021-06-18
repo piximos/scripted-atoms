1f40830faa12c50b40bed12a2e4b127c38cde864
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-06-18 11:48:47 +0100

    Made changes to project

--- ci-cd/scripts/pipeline-broadcaster.sh
+++ ci-cd/scripts/pipeline-broadcaster.sh
@@ -2,9 +2,11 @@
 
 if [[ "${SUCCESS}" == "true" ]]; then
   SLACK_MSG="Successfully deployed the following images :"
+  DISCORD_MSG="Successfully deployed the following images :"
   SLACK_COLOR="#87ebaa"
 else
   SLACK_MSG="Failed to build and deploy the following images :"
+  DISCORD_MSG="Failed to build and deploy the following images :"
   SLACK_COLOR="#eb87b7"
 fi
 
@@ -13,12 +15,18 @@ IFS=', ' read -r -a TAGS <<<"$(echo "$TAGS")"
 slack_artifacts=""
 for tag in "${TAGS[@]}"; do
   slack_artifacts="$slack_artifacts\\n- $IMAGE:\`$tag\`"
+  DISCORD_MSG="$DISCORD_MSG \`$tag\`"
 done
 
 docker pull scriptedatom/ssa-pipeline-slacker:latest
+docker pull scriptedatom/ssa-pipeline-discorder:latest
 docker run --rm \
   -e SLACK_WEBHOOK_URL="$SLACK_WEBHOOK_URL" \
   -e SLACK_MSG="$SLACK_MSG" \
   -e SLACK_COLOR="$SLACK_COLOR" \
   -e SLACK_ARTIFACTS="$slack_artifacts" \
   scriptedatom/ssa-pipeline-slacker:latest
+docker run --rm \
+  -e DISCORD_WEBHOOK_URL="$DISCORD_WEBHOOK_URL" \
+  -e DISCORD_MSG="$DISCORD_MSG" \
+  scriptedatom/ssa-pipeline-discorder:latest
