971a40f25efb6287b6a2578c25ec5c9d6018247d
Youssef Dhraief <dhraief.youssef@gmail.com>
2021-07-14 11:31:48 +0100

    Reduced the number of stages

--- ci-cd/scripts/pipeline-slacker.sh
+++ ci-cd/scripts/pipeline-slacker.sh
@@ -5,18 +5,23 @@ TAGS="$TAGS sa-$(git describe | grep -Eo '^([0-9]+(\-|\.)[0-9]+((\-|\.)[0-9]+)?)
 if [[ "${SUCCESS}" == "true" ]]; then
   SLACK_MSG="Successfully deployed the following images :"
   SLACK_COLOR="#87ebaa"
+  DISCORD_MSG="Successfully deployed the following tags :"
 else
   SLACK_MSG="Failed to build and deploy the following images :"
   SLACK_COLOR="#eb87b7"
+  DISCORD_MSG="Failed to build and deploy the following tags :"
 fi
 
 slack_artifacts=""
 for tag in ${TAGS}; do
   slack_artifacts="$slack_artifacts\\n- $IMAGE:\`$tag\`"
+  DISCORD_MSG="$DISCORD_MSG \\n > $IMAGE:\`$tag\`"
 done
 
 export SLACK_MSG="$SLACK_MSG"
 export SLACK_COLOR="$SLACK_COLOR"
 export SLACK_ARTIFACTS="$slack_artifacts"
+export DISCORD_MSG="$DISCORD_MSG"
 
 ./scripted-simple-atoms/pipeline-slacker-atom/runner.sh
+./scripted-simple-atoms/pipeline-discorder-atom/runner.sh
